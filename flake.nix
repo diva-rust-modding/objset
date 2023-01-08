e4f25ccf0f610d71edc034d1095b5520ab4462eb
waelwindows <waelwindows9922@gmail.com>
2023-01-08 19:46:56 +0300

    temp

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     rust-overlay,
   }: let
     emptyOverlay = final: prev: {};
-    objset-drv = pkgs:
+    objset-drv = pkgs: override:
       pkgs.rustPlatform.buildRustPackage {
         pname = "objset";
         version = "v0.1.0";
@@ -28,7 +28,7 @@
           # Why I yes, I would like not writing the hash of my Cargo.lock very much.
           lockFile = ./Cargo.lock;
         };
-      };
+      } // override;
     objset-python-drv = pkgs: pythonPackages:
       pythonPackages.buildPythonPackage rec {
         pname = "objset";
@@ -63,9 +63,44 @@
       system: let
         overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {inherit system overlays;};
+        pkgs-cross-mingw = import nixpkgs {
+          inherit system;
+          crossSystem = {
+              config = "x86_64-w64-mingw32";
+            };
+        };
+        mingw_w64_cc = pkgs-cross-mingw.stdenv.cc;
+        mingw_w64 = pkgs-cross-mingw.windows.mingw_w64;
+        mingw_w64_pthreads_w_static = pkgs-cross-mingw.windows.mingw_w64_pthreads.overrideAttrs (oldAttrs: {
+          # TODO: Remove once / if changed successfully upstreamed.
+          configureFlags = (oldAttrs.configureFlags or []) ++ [
+            # Rustc require 'libpthread.a' when targeting 'x86_64-pc-windows-gnu'.
+            # Enabling this makes it work out of the box instead of failing.
+            "--enable-static"
+          ];
+        });
       in rec {
         packages = rec {
-          objset = objset-drv pkgs;
+          objset = objset-drv pkgs {};
+          objset-w64-gnu = pkgs-cross-mingw.rustPlatform.buildRustPackage {
+            pname = "objset-w64-gnu";
+            version = "v0.1.0";
+
+            src = ./.;
+
+            cargoLock = {
+              # Why I yes, I would like not writing the hash of my Cargo.lock very much.
+              lockFile = ./Cargo.lock;
+            };
+            target = "x86_64-pc-windows-gnu";
+            doCheck=false;
+
+            # buildInputs = with pkgs; [
+            #   pkgsCross.mingwW64.stdenv.cc
+            #   pkgsCross.mingwW64.buildPackages.binutils
+            #   pkgsCross.mingwW64.windows.pthreads
+            # ];
+          };
           objset-python = objset-python-drv pkgs pkgs.python3Packages;
           default = objset;
         };
@@ -77,9 +112,44 @@
             gcc
           ];
 
-          RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
-            extensions = ["rust-src"];
-          }}/lib/rustlib/src/rust/library";
+          # RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
+          #   extensions = ["rust-src"];
+          # }}/lib/rustlib/src/rust/library";
+          # PYO3_MINGW_DLLTOOL="${pkgs.llvmPackages.bintools-unwrapped}/bin/llvm-dlltool";
+          buildInputs = with pkgs; [
+            maturin
+            rust-analyzer
+            # for targetting x86_64-pc-windows-gnu
+            # (binutils.override { withAllTargets=true; })
+            pkgsCross.mingwW64.buildPackages.binutils
+            # pkgsCross.mingwW64.stdenv.cc
+            mingw_w64_cc
+            # binutils-unwrapped-all-targets
+            # for targetting x86_64-pc-windows-msvc
+            llvmPackages.bintools
+            (pkgs.python3.withPackages (p:
+              with p; [
+                cffi
+              ]))
+          ];
+          RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+            mingw_w64
+            mingw_w64_pthreads_w_static
+          ]);
+        };
+        devShells.macos = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [
+            pkgsCross.aarch64-darwin.buildPackages.gccStdenv
+            (pkgs.rust-bin.stable.latest.default.override {
+              extensions = ["rust-src" "cargo" "rustc"];
+            })
+            gcc
+          ];
+
+          # RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
+          #   extensions = ["rust-src"];
+          # }}/lib/rustlib/src/rust/library";
+          # PYO3_MINGW_DLLTOOL="${pkgs.llvmPackages.bintools-unwrapped}/bin/llvm-dlltool";
           buildInputs = with pkgs; [
             maturin
             rust-analyzer
